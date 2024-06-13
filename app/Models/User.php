0f1197a5e378703fe50e515a596fb726ae17b9d3
hideki74 <07hidek04@gmail.com>
2024-06-13 15:58:10 +0900

    users 追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +45,48 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function articles(): HasMany {
+        return $this->hasMany(Article::class);
+    }
+
+    public function followers(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'follows', 'followee_id', 'follower_id')->withTimeStamps();
+    }
+
+    public function followings(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
+    public function likes(): BelongsToMany {
+        return $this->belongsToMany(Article::class, 'likes')->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user): bool {
+        return $user ? (bool)$this->followers->where('id', $user->id)->count() : false;
+    }
+
+    public function getCountFollowersAttribute(): int {
+        return $this->followers->count();
+    }
+
+    public function getCountFollowingsAttribute(): int {
+        return $this->followings->count();
+    }
+
+    public function getCountArticlesAttribute(): int {
+        return $this->articles->count();
+    }
+
+    public function getCountLikesAttribute(): int {
+        return $this->likes->count();
+    }
+
+    public function getCountGetLikesAttribute(): int {
+        $liked_cnt = 0;
+        foreach ($this->articles as $article) {
+            $liked_cnt += $article->likes->count();
+        }
+        return $liked_cnt;
     }
 }
