80197fd61849546d97984af4f6d8c503d34d0db2
hideki74 <07hidek04@gmail.com>
2024-07-16 15:46:43 +0900

    DBのデータからjsonファイルを作る関数の追加 ・コントローラー上ではなく、モデル上にjsonを作成する関数を追加

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\CardList;
 use App\Models\User;
 use App\Models\Card;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,15 +21,8 @@ class CardController extends Controller
     // カードのデータをjsonをstringに変換して返す
     // カードのページを読み込んだとき最初に呼ばれる
     public function initCards(Request $request){
-        $card_lists = CardList::where('user_id', $request->user()->id)->get();
-
-        $lists = [];
-        foreach ($card_lists as $card_list) {
-            $cards = [];
-            Card::where('list_id', $card_list->id)->get();
-        }
-        // $cards = Card::get0rCreateCardsJson($request->user()->id);
-        // return $cards;
+        $cards = CardList::createCardJson($request->user()->id);
+        return $cards;
     }
 
     // put
--- app/Models/CardList.php
+++ app/Models/CardList.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,28 +20,26 @@ class CardList extends Model
         return $this->hasMany(Card::class);
     }
 
-    public function createCardJson(): string {
-
-    }
-
-    public function parseCardJson() {
-        $json = '[
-            {
-                "cards": [],
-                "title": "ff"
-            },
-            {
-                "cards": [
-                    {
-                        "body": "ff"
-                    },
-                    {
-                        "body": "aaaa"
-                    }
-                ],
-                "title": "fffe"
+    public static function createCardJson(int $user_id): string {
+        $card_lists = self::where('user_id', $user_id)->get();
+        $lists = [];
+        foreach ($card_lists as $index => $card_list) {
+            // カードを追加
+            $cards = Card::where('list_id', $card_list->id)->get();
+            $cards_array = [];
+            foreach ($cards as $card_index => $card) {
+                $cards_array += [$card_index => ['body' => $card->card_body]];
             }
-        ]';
-    }
 
+            // lists配列にタイトルとカードを追加
+            $lists += [$index => [
+                'cards' => $cards_array,
+                'title' => $card_list->list_title,
+            ]]; 
+        }
+
+        // lists配列をjsonに変換
+        $card_json = json_encode($lists);
+        return $card_json;
+    }
 }
